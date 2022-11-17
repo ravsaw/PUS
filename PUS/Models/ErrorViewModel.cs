29ef34da825c3473c77a7d999e1e51a2367bcaf8
rav <rafal.sawczuk@gmail.com>
2022-11-17 09:24:42 +0100

    Dodaj pliki projektów.

---
+++ PUS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PUS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
